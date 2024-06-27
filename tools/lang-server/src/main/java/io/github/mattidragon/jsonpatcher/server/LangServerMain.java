9ee2a6963653958c8ad325f742c1a1fb22e95fa0
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2024-06-27 10:43:00 +0300

    Switch to socket transport to avoid issues with random logging

--- tools/lang-server/src/main/java/io/github/mattidragon/jsonpatcher/server/LangServerMain.java
+++ tools/lang-server/src/main/java/io/github/mattidragon/jsonpatcher/server/LangServerMain.java
@@ -5,28 +5,34 @@ import org.eclipse.lsp4j.launch.LSPLauncher;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.net.Socket;
 import java.util.concurrent.ExecutionException;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class LangServerMain {
     public static void main(String[] args) {
-//        hackIO();
-
-        if (Boolean.parseBoolean(System.getProperty("jsonpatcher.ls.disable_log", "false"))) {
-            LogManager.getLogManager().reset();
-            var globalLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-            globalLogger.setLevel(Level.OFF);
+        int port = 18092;
+        for (var arg : args) {
+            if (arg.startsWith("--socket=")) {
+                port = Integer.parseInt(arg.substring("--socket=".length()));
+            } else {
+                System.err.println("Unexpected argument: " + arg);
+            }
         }
         
+        try (var socket = new Socket("localhost", port)) {
+            runServer(socket.getInputStream(), socket.getOutputStream());
+        } catch (IOException e) {
+            System.err.println("Failed to start socket");
+            e.printStackTrace(System.err);
+            System.exit(2);
+        }
+    }
+
+    private static void runServer(InputStream in, OutputStream out){
         var server = new JsonPatcherLanguageServer();
-        var launcher = LSPLauncher.createServerLauncher(server, System.in, System.out);
+        var launcher = LSPLauncher.createServerLauncher(server, in, out);
         server.connect(launcher.getRemoteProxy());
-        
+
         try {
             launcher.startListening().get();
         } catch (InterruptedException | ExecutionException e) {
@@ -34,61 +40,5 @@ public class LangServerMain {
             e.printStackTrace(System.err);
             System.exit(1);
         }
-    }
-
-    private static void hackIO() {
-        var oldOut = System.out;
-        OutputStream file;
-        try {
-            //noinspection resource
-            file = Files.newOutputStream(Path.of("log.txt"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.setOut(new PrintStream(new OutputStream() {
-            @Override
-            public void write(int b) throws IOException {
-                oldOut.write(b);
-                file.write(b);
-            }
-
-            @Override
-            public void flush() throws IOException {
-                oldOut.flush();
-                file.flush();
-            }
-        }));
-        var oldErr = System.err;
-        OutputStream file2;
-        try {
-            //noinspection resource
-            file2 = Files.newOutputStream(Path.of("err.txt"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.setErr(new PrintStream(new OutputStream() {
-            @Override
-            public void write(int b) throws IOException {
-                oldErr.write(b);
-                file2.write(b);
-            }
-        }));
-
-        OutputStream file3;
-        try {
-            //noinspection resource
-            file3 = Files.newOutputStream(Path.of("in.txt"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        var oldIn = System.in;
-        System.setIn(new InputStream() {
-            @Override
-            public int read() throws IOException {
-                var val = oldIn.read();
-                file3.write(val);
-                return val;
-            }
-        });
     }
 }
