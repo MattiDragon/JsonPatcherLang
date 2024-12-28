ba614757e203390028ab55b65705158775b693e5
MattiDragon <matias.koivikko@gmail.com>
2024-12-28 11:51:34 +0200

    Fix compile error

--- tools/formatter/src/test/java/dev/mattidragon/jsonpatcher/formatter/test/FormatValidator.java
+++ tools/formatter/src/test/java/dev/mattidragon/jsonpatcher/formatter/test/FormatValidator.java
@@ -1,19 +1,19 @@
 package dev.mattidragon.jsonpatcher.formatter.test;
 
 import dev.mattidragon.jsonpatcher.formatter.printer.ExpressionPrinter;
+import dev.mattidragon.jsonpatcher.formatter.printer.PrettyPrintOptions;
 import dev.mattidragon.jsonpatcher.formatter.printer.PrettyPrinter;
-import dev.mattidragon.jsonpatcher.lang.LangConfig;
 import dev.mattidragon.jsonpatcher.lang.ast.expression.Expression;
 import dev.mattidragon.jsonpatcher.lang.test.TestUtils;
 import org.junit.jupiter.api.Assertions;
 
 public class FormatValidator {
-    private static final LangConfig CONFIG = new LangConfig(LangConfig.StackTraceMode.JAVA);
+    private static final PrettyPrintOptions OPTIONS = PrettyPrintOptions.builder().build();
 
     private FormatValidator() {}
 
     public static void validate(Expression expression) {
-        var printer = new PrettyPrinter();
+        var printer = new PrettyPrinter(OPTIONS);
         ExpressionPrinter.prettyPrint(expression, printer);
         var code = printer.getOutput();
 
